4bf1c0bd1fcf735955cf8f4eb4bbf0f9ee6b1968
nmo <naki.md@gmail.com>
2021-01-17 02:13:31 +0100

    feature(fetching pokemon full details)

--- pages/pokemons/index.jsx
+++ pages/pokemons/index.jsx
@@ -1,41 +1,62 @@
 import Head from 'next/head';
-import react from 'react';
+import { useEffect, useState } from 'react';
 
-export default class Home extends react.Component {
-    constructor(props) {
-        super(props)
-        this.state = {
-            error: null,
-            isLoaded: false,
-            items: []
-        }
-    }
 
-    async componentDidMount() {
-        const fetched = await fetch("//pokeapi.co/api/v2/pokemon?limit=100&offset=200")
-        const json = await fetched.json()
-        const data = [...json.results]
-        console.dir({data: data})
-        this.setState({
-            ...this.state,
-            items: data 
+export default function Spookymons() {
+    const [pokemonUrlList, setPokemonList] = useState([]);
+    const [pokemonData, setPokemonData] = useState([]);
+    const [isLoaded, setLoaded] = useState(false) // TODO
+
+
+    useEffect(() => {
+        fetch("//pokeapi.co/api/v2/pokemon?limit=100&offset=200").then(fetched => {
+            fetched.json().then(data => {
+                console.table(data.results.slice(0, 4))
+                setPokemonList(data.results)
+            })
         })
-    }
-    
-    render() {
-        return <react.Fragment>
-            <div>
-                <Head>
-                    <title>Spookymons!</title>
-                </Head>
-                <h1>Pokemon list</h1>
-                <ul>
-                    {
-                        !!this.state.items && this.state.items.length > 0 && this.state.items.map(item =>
-                            <li key={item.name}><pre>{item.name}</pre></li>)
-                    }
-                </ul>
-            </div>
-        </react.Fragment>
-    }
+    }, [])
+
+    useEffect(async () => {
+        const result = await pokemonUrlList.map(async pokemonUrlList => ({
+            [pokemonUrlList.url]: (
+                await (await fetch(pokemonUrlList.url)).json()
+            )
+        }))
+        console.log({ result })
+        setPokemonData(await Promise.allSettled(result))
+    }, [pokemonUrlList])
+
+
+    return <>
+        <div>
+            <pre>{JSON.stringify(pokemonData, false, 2)}</pre>
+            <Head>
+                <title>Spookymons!</title>
+            </Head>
+            <h1>Pokemon list</h1>
+            {pokemonUrlList && pokemonUrlList.map(pokemon => <div key={pokemon.name}>
+                <h2>{pokemon.name}</h2>
+                <h3>{pokemon.url}</h3>
+            </div>)}
+            {/* <pre>{JSON.stringify(pokemonList, false, 2)}</pre> */}
+        </div>
+    </>
 }
+
+// const getPokemonData = ({ pokemonUrl }) => {
+//     const [pokemonData, setPokemonData] = useState({});
+
+//     useEffect(() => {
+//         fetch(pokemonUrl).then(fetched => {
+//             fetched.json().then(data => {
+//                 console.table(data.results.slice(0, 4))
+//                 setPokemonData(data.results)
+//             })
+//         })
+//     }, [])
+
+//     return <>
+//         <pre>{JSON.stringify(pokemonData, false, 2)}</pre>
+//     </>
+// }
