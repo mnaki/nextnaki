f60baf1e2c010d6bf4d71560a4c754524e642780
naki <naki.md@gmail.com>
2021-01-22 10:52:15 +0100

    sadad

---
+++ pages/_app.tsx
@@ -0,0 +1,20 @@
+import '../styles/globals.css'
+
+import Cors from 'cors'
+// import initMiddleware from '../../lib/init-middleware'
+
+// Initialize the cors middleware
+// const cors = initMiddleware(
+//     // You can read more about the available options here: https://github.com/expressjs/cors#configuration-options
+//     Cors({
+//         // Only allow requests with GET, POST and OPTIONS
+//         methods: ['GET', 'POST', 'OPTIONS'],
+//     })
+// )
+
+function MyApp({ Component, pageProps }) {
+    await cors(req, res)
+    return <Component {...pageProps} />
+}
+
+export default MyApp
---
+++ pages/index.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+    return <div>
+        Hello world
+         <ul>
+            <li><a href={"/pokemons"}>See pokemons</a></li>
+        </ul>
+    </div>
+}
